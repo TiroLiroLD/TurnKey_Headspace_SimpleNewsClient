a7a2be58aae20284f089860d14852c37bca63e6d
Leone Souza <leone.souza@edu.unirio.br>
2024-07-29 05:36:32 -0300

    add article bookmark property

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -8,37 +8,38 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:get_it/get_it.dart' as _i1;
-import 'package:injectable/injectable.dart' as _i2;
-import 'package:simple_news_client/helpers/database_helper.dart' as _i7;
-import 'package:simple_news_client/providers/news_provider.dart' as _i8;
-import 'package:simple_news_client/repositories/news_repository.dart' as _i4;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+import 'package:simple_news_client/helpers/database_helper.dart' as _i1056;
+import 'package:simple_news_client/providers/news_provider.dart' as _i707;
+import 'package:simple_news_client/repositories/news_repository.dart' as _i625;
 import 'package:simple_news_client/repositories/news_repository_interface.dart'
-    as _i3;
-import 'package:simple_news_client/services/news_service.dart' as _i6;
-import 'package:simple_news_client/services/news_service_interface.dart' as _i5;
+    as _i847;
+import 'package:simple_news_client/services/news_service.dart' as _i952;
+import 'package:simple_news_client/services/news_service_interface.dart'
+    as _i360;
 
-extension GetItInjectableX on _i1.GetIt {
+extension GetItInjectableX on _i174.GetIt {
 // initializes the registration of main-scope dependencies inside of GetIt
-  _i1.GetIt init({
+  _i174.GetIt init({
     String? environment,
-    _i2.EnvironmentFilter? environmentFilter,
+    _i526.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i2.GetItHelper(
+    final gh = _i526.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
-    gh.factory<_i3.INewsRepository>(() => _i4.NewsRepository(
+    gh.factory<_i847.INewsRepository>(() => _i625.NewsRepository(
           apiKey: gh<String>(instanceName: 'apiKey'),
           baseUrl: gh<String>(instanceName: 'baseUrl'),
         ));
-    gh.factory<_i5.INewsService>(() => _i6.NewsService(
-          newsRepository: gh<_i3.INewsRepository>(),
-          databaseHelper: gh<_i7.DatabaseHelper>(),
+    gh.factory<_i360.INewsService>(() => _i952.NewsService(
+          newsRepository: gh<_i847.INewsRepository>(),
+          databaseHelper: gh<_i1056.DatabaseHelper>(),
         ));
-    gh.factory<_i8.NewsProvider>(
-        () => _i8.NewsProvider(newsService: gh<_i5.INewsService>()));
+    gh.factory<_i707.NewsProvider>(
+        () => _i707.NewsProvider(newsService: gh<_i360.INewsService>()));
     return this;
   }
 }
